cef0f45de6014a2745dc9ed976218c34506cb568
AbooMinister25 <aboominister@gmail.com>
2021-02-17 19:24:33 -0500

    New Commit

---
+++ arc/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="arc",  # Replace with your own username
+    version="1.0.0",
+    author="Rayyan Cyclegar",
+    author_email="aboominister@gmail.com",
+    description="A Python micro web framework for creating dynamic websites.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AbooMinister25/arc",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
