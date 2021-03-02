5a280e9cd073b35d63f249e0d9efcde95c6da4bd
AbooMinister25 <aboominister@gmail.com>
2021-03-02 11:23:52 -0500

    New Commit

--- arc/sessions.py
+++ arc/sessions.py
@@ -3,29 +3,34 @@ The below is COMING SOON
 """
 
 
-
-# from arc.middleware import Middleware
-# import secrets
-
-
-# class SessionHandler:
-#     def __init__(self, app):
-#         self.app = app
-#         self.lifetime = self.app.to_seconds(1, "hour")
-
-#         self._dict = {}
-
-#     def init_session(self, response):
-#         key = secrets.token_urlsafe(20)
-#         response.set_cookie("secret_key", key,
-#                             httponly=True, max_age=self.lifetime)
-#         return response
+from arc.middleware import Middleware
+import secrets
 
 
-# class SessionMiddleware(Middleware):
-#     def process_request(req):
-#         ...
+class SessionHandler:
+    def __init__(self, app):
+        self.app = app
+        self.lifetime = self.app.to_seconds(1, "hour")
 
-#     def process_response(req, res):
-#         key = secrets.token_urlsafe(20)
-#         res.set_cookie("secret_key", key)
+        self._dict = {}
+
+    def init_session(self, response) -> None:
+        key = secrets.token_urlsafe(20)
+        response.set_cookie("secret_key", key,
+                            httponly=True, max_age=self.lifetime)
+        self._dict[key] = {}
+
+    def __setitem__(self, key, value):
+        self._dict[key] = value
+
+    def __getitem__(self, key):
+        return self._dict[key]
+
+
+class SessionMiddleware(Middleware):
+    def process_request(self, req):
+        ...
+
+    def process_response(self, req, res):
+        key = secrets.token_urlsafe(20)
+        res.set_cookie("secret_key", key)
