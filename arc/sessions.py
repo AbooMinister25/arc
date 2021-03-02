c6d794546ff9875f593f909013f81ae35e751bee
AbooMinister25 <aboominister@gmail.com>
2021-03-02 13:50:53 -0500

    New Commit

--- arc/sessions.py
+++ arc/sessions.py
@@ -4,33 +4,34 @@ The below is COMING SOON
 
 
 from arc.middleware import Middleware
+from starlette.requests import Request
 import secrets
 
 
 class SessionHandler:
-    def __init__(self, app):
+    def __init__(self, app, secret_key=None):
         self.app = app
         self.lifetime = self.app.to_seconds(1, "hour")
+
+        self.secret_key = secrets.token_urlsafe()
 
         self._dict = {}
 
     def init_session(self, response) -> None:
-        key = secrets.token_urlsafe(20)
-        response.set_cookie("secret_key", key,
+        key = secrets.token_urlsafe(50)
+        response.set_cookie("secret_key", key+self.secret_key,
                             httponly=True, max_age=self.lifetime)
+
         self._dict[key] = {}
 
-    def __setitem__(self, key, value):
-        self._dict[key] = value
-
-    def __getitem__(self, key):
-        return self._dict[key]
+    def __call__(self, request):
+        if request.cookies.get("secret_key") in self._dict:
+            return self._dict[request.cookies.get("secret_key")]
 
 
-class SessionMiddleware(Middleware):
-    def process_request(self, req):
-        ...
+# class SessionMiddleware(Middleware):
+#     def process_request(self, req):
+#         self.app.scope["current_secret"] = req.cookies.get("secret_key")
 
-    def process_response(self, req, res):
-        key = secrets.token_urlsafe(20)
-        res.set_cookie("secret_key", key)
+#     def process_response(self, req, res):
+#         pass
